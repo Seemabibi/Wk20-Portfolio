2562353aa332b59af63b2e85768819aeb2b68e1c
seemabibi <seema.bibi@hotmail.co.uk>
2023-03-06 16:32:45 +0000

    email and github link

---
+++ pages/ContactMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ContactMe() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>
+        <ul>
+        <li>Email Me - seema.bibi@hotmail.co.uk</li>
+        <li>Visit my Github repo - <a href="https://github.com/cherry-aisha"> My Github Repo </a></li>
+        <li>Message Me on Instagram<a href="https://instagram.com/cherry.aisha?igshid=YmMyMTA2M2Y="> My Instagram </a></li>
+        </ul>
+      </p>
+    </div>
+  );
+}
